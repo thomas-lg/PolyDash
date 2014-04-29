48b497da1e77a37d32108cc25326eb3d3289aeae
Florian <florian.bonniec22@gmail.com>
2014-04-29 11:12:40 +0200

    Ajout de la méthode alive permettant de savoir si le personnage est mort ou vivant

--- PolyDash/src/polytech/polydash/draughtboardmanagement/Character.java
+++ PolyDash/src/polytech/polydash/draughtboardmanagement/Character.java
@@ -1,10 +1,21 @@
 package polytech.polydash.draughtboardmanagement;
 
 public class Character extends Block {
-
+	private boolean alive;
+	
+	public Character(){
+		this.setAlive(true);
+	}
 	@Override
 	public String toString() {
 		return "P";
 	}
+	public boolean isAlive() {
+		return alive;
+	}
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+	
 
 }
